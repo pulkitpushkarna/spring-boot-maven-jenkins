d34d5c6f4b0fcc583d09229ce5b17fc29d6d19c7
Pulkit <pulkit.pushkarna@tothenew.com>
2022-01-22 09:13:14 +0530

    remove deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export BUILD_ID=dontKillMe
-app_id=$(ps -ef | grep 'jenkins-maven-0.0.1-SNAPSHOT.jar' | grep -v 'grep' | awk '{ printf $2 }')
-kill -9 $app_id
-nohup java -jar ./build/libs/basic-app-0.0.1-SNAPSHOT.jar &
