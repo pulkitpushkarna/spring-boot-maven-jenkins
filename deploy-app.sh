477fb4226802e90f38a2f3f753a709f3a6b62fcd
Pulkit <pulkit.pushkarna@tothenew.com>
2022-01-22 09:19:00 +0530

    modified deploy-app.sh

--- deploy-app.sh
+++ deploy-app.sh
@@ -2,4 +2,4 @@
 export BUILD_ID=dontKillMe
 app_id=$(ps -ef | grep 'basic-app-0.0.1-SNAPSHOT.jar' | grep -v 'grep' | awk '{ printf $2 }')
 kill -9 $app_id
-nohup java -jar ./build/libs/basic-app-0.0.1-SNAPSHOT.jar &
+nohup java -jar ./target/jenkins-maven-0.0.1-SNAPSHOT.jar &
